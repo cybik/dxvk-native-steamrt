d35eaa8110e0a0c34a00e79af583573db82771d5
Renaud Lepage <cybik@users.noreply.github.com>
2020-06-30 09:37:14 -0700

    No need for sudo
    Assume that your users know what they need to do to use docker

--- create_container.sh
+++ create_container.sh
@@ -9,14 +9,14 @@ pushd tmp
   wget https://repo.steampowered.com/steamrt-images-scout/snapshots/latest-steam-client-general-availability/com.valvesoftware.SteamRuntime.Sdk-amd64,i386-scout-sysroot.tar.gz
   wget https://release.froggi.es/scout-0.20200214.0-gcc-9.2-binaries.tar.xz
 
-  sudo docker build \
+  docker build \
     -f com.valvesoftware.SteamRuntime.Sdk-amd64,i386-scout-sysroot.Dockerfile \
     -t dxvk-steamrt-base:latest \
     .
 
   tar -xJf scout-0.20200214.0-gcc-9.2-binaries.tar.xz
 
-  sudo docker build \
+  docker build \
     -f ../dxvk-native-steamrt.Dockerfile \
     -t dxvk-steamrt:latest \
     .
